9da18d908c08cb37f7925a1018dc324f79ab3fdd
Enzo <enzoarielherreraortega97@gmail.com>
2024-09-22 18:52:42 -0300

    ejercicio 1

--- Condicionales y Repetitivos/src/ParteB/Ejercicio1/Ejercicio1.java
+++ Condicionales y Repetitivos/src/ParteB/Ejercicio1/Ejercicio1.java
@@ -1,7 +1,23 @@
 package ParteB.Ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("cuanto dinero gana el joven? ");
+        int sueldo = scanner.nextInt();
+        System.out.println("cuanto porsentaje del sueldo quiere ahorrar por mes? ");
+        float ahorro = scanner.nextInt();
+        System.out.println("cuantos meses va a ahorrar? ");
+        int meses = scanner.nextInt();
+        float ahorroAcumulado =0;
+        for (int i = 1; i <= meses; i++) {
+            float ahorrado = sueldo * (ahorro / 100);
+            ahorroAcumulado += ahorrado;
+
+        }
+        System.out.println("En "+ meses + " mese/s ahorro un total de $"+ ahorroAcumulado );
     }
 }
